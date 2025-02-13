844b5e74230e4a37bc643512065eaae59abccbe0
Maria Stepanova <stepanova.m.v.91@gmail.com>
2025-02-13 17:50:17 +0300

    First task

---
+++ src/by/homework/java/task1/Main.java
@@ -0,0 +1,6 @@
+package by.homework.java.task1;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Степанова Мария Валерьевна, тестировщик, дата выдачи домашенего задания 06 февраля 2025 года 23:04");}
+}
